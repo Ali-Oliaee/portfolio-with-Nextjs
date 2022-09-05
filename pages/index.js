a9083046facb75cc15d03a4500041620aeca9841
ali <alioliaee31@gmail.com>
2022-09-05 22:25:50 +0430

    add tsparticles

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { useTranslation } from "next-i18next";
 import PageWrapper from "../components/page-wrapper";
-
+import Particles from "react-tsparticles";
+import { loadTrianglesPreset } from "tsparticles-preset-triangles";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 export async function getStaticProps({ locale }) {
@@ -13,9 +14,25 @@ export async function getStaticProps({ locale }) {
 
 export default function Home() {
   const { t } = useTranslation("common");
+  const customInit = (engine) => loadTrianglesPreset(engine);
+  const options = {
+    preset: "triangles",
+    background: {
+      color: {
+        value: "transparent",
+      },
+    },
+    particles: {
+      number: { value: 20 },
+      move: {
+        speed: 1.5,
+      },
+    },
+  };
 
   return (
     <PageWrapper className="home-page">
+      <Particles options={options} init={customInit} />
       <h1 className="name">
         {t("name")} <span className="family">{t("family")}</span>
       </h1>
