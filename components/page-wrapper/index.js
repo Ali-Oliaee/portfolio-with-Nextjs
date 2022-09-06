e493cb03fb4331ede43267273fa8f0c81b4105e1
ali <alioliaee31@gmail.com>
2022-09-06 15:24:31 +0430

    change dir base on locale

--- components/page-wrapper/index.js
+++ components/page-wrapper/index.js
@@ -1,9 +1,12 @@
 import Footer from "../footer.js";
 import Header from "../header";
+import { useRouter } from "next/router";
 
 function PageWrapper({ children }) {
+  const { locale } = useRouter();
+
   return (
-    <div className="page-wrapper">
+    <div className="page-wrapper" dir={locale === "fa" ? "rtl" : "ltr"}>
       <Header />
       {children}
       <Footer />
--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,16 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps, locale: ctx?.locale || "en" };
+  }
   render() {
     return (
-      <Html>
+      <Html
+        dir={this.props.locale === "fa" ? "rtl" : "ltr"}
+        lang={this.props.locale}
+      >
         <Head>
           <link
             href="https://fonts.googleapis.com/css2?family=Andika&display=swap"
