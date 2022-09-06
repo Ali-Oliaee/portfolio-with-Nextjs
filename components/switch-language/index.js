4fd2f77dda44cf8aa9e5844ad8ce36806221c43f
ali <alioliaee31@gmail.com>
2022-09-06 15:15:28 +0430

    debug locales

---
+++ components/switch-language/index.js
@@ -0,0 +1,39 @@
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import { useTranslation } from "next-i18next";
+import router from "next/router";
+
+function SwitchLanguage({ className }) {
+  const { t, i18n } = useTranslation();
+
+  const changeLocale = (locale) => {
+    router.push(
+      {
+        route: router.pathname,
+        query: router.query,
+      },
+      router.asPath,
+      { locale }
+    );
+  };
+
+  return (
+    <div className={className ?? "switch-language"}>
+      <span className="language">{t("language")}:</span>
+      <FormControl variant="standard">
+        <Select
+          value={(typeof window !== "undefined" && router.locale) || "en"}
+          IconComponent={() => null}
+          onChange={(e) => changeLocale(e.target.value)}
+          className="select"
+        >
+          <MenuItem value="en">en</MenuItem>
+          <MenuItem value="fa">fa</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
+
+export default SwitchLanguage;
